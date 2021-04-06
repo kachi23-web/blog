21cf62b8bd5e544559574b74cb7ab6f0f86e36e9
Onyeka Nwakobi <onyeka@aspilosfoundation.org.ng>
2021-04-06 22:32:15 +0100

    working on the admin module

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,10 +11,9 @@ const mix = require('laravel-mix');
  |
  */
 
-
- mix.sass('resources/sass/app.scss', 'public/css')
- .sass('resources/sass/admin/sb-admin.scss', 'public/css/admin');
-
 mix.js('resources/js/app.js', 'public/js')
-    .vue();
-    
+.js('resources/js/admin/sb-admin.js', 'public/js/admin')
+    .vue()
+    .sass('resources/sass/app.scss', 'public/css')
+    .sass('resources/sass/admin/sb-admin.scss', 'public/css/admin')
+    .sourceMaps();
